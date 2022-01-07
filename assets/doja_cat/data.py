e116ec7d28f779c51abc13b448dadd3a6eddae93
enciphered-nft <encipherednft@gmail.com>
2022-01-07 22:41:06 +0400

    doja cat

---
+++ assets/doja_cat/data.py
@@ -0,0 +1,800 @@
+import datetime
+
+METADATA = {
+    "name": "Amala Ratna Zandile Dlamini",
+    "born": datetime.datetime(1995, 10, 21),
+    "occupation": ["Singer", "rapper", "songwriter", "record producer", "dancer"],
+    "years_active": "2012-present",
+    "total_award_wins": 64,
+    "total_nominations": 163,
+    "awards": [
+ {
+   "Organization": "American Music Awards",
+   "Year": 2020,
+   "Category": "New Artist of the Year",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[104]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2020,
+   "Category": "Favorite Female Soul/R&B Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[104]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2020,
+   "Category": "Favorite Soul/R&B Album",
+   "Nominated work": "Hot Pink",
+   "Result": "Nominated",
+   "Ref.": "[104]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2020,
+   "Category": "Video of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[104]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2021,
+   "Category": "Collaboration of the Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Won",
+   "Ref.": "[222]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Pop/Rock Song",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[222]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Pop/Rock Female Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[222]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Soul/R&B Female Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[222]"
+ },
+ {
+   "Organization": "American Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Soul/R&B Album",
+   "Nominated work": "Planet Her",
+   "Result": "Won",
+   "Ref.": "[222]"
+ },
+ {
+   "Organization": "ARIA Music Awards",
+   "Year": 2021,
+   "Category": "Best International Artist",
+   "Nominated work": "Planet Her",
+   "Result": "Nominated",
+   "Ref.": "[223]"
+ },
+ {
+   "Organization": "BET Awards",
+   "Year": 2020,
+   "Category": "Video of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[87]"
+ },
+ {
+   "Organization": "BET Awards",
+   "Year": 2020,
+   "Category": "Best Female Hip Hop Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[87]"
+ },
+ {
+   "Organization": "BET Awards",
+   "Year": 2021,
+   "Category": "Best Female Hip Hop Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[224]"
+ },
+ {
+   "Organization": "BET Hip Hop Awards",
+   "Year": 2021,
+   "Category": "Best Hip Hop Video",
+   "Nominated work": "Best Friend (with Saweetie)",
+   "Result": "Nominated",
+   "Ref.": "[225]"
+ },
+ {
+   "Organization": "BET Hip Hop Awards",
+   "Year": 2021,
+   "Category": "Best Live Performer",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[225]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2020,
+   "Category": "Top R&B Song",
+   "Nominated work": "Juicy (with Tyga)",
+   "Result": "Nominated",
+   "Ref.": "[93]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2021,
+   "Category": "Top New Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[226]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2021,
+   "Category": "Top R&B Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[226]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2021,
+   "Category": "Top R&B Female Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[226]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2021,
+   "Category": "Top R&B Album",
+   "Nominated work": "Hot Pink",
+   "Result": "Nominated",
+   "Ref.": "[226]"
+ },
+ {
+   "Organization": "Billboard Music Awards",
+   "Year": 2021,
+   "Category": "Top R&B Song",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[226]"
+ },
+ {
+   "Organization": "BMI Pop Awards",
+   "Year": 2021,
+   "Category": "Award-Winning Songs",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[227]"
+ },
+ {
+   "Organization": "BMI Pop Awards",
+   "Year": 2021,
+   "Category": "Award-Winning Songs",
+   "Nominated work": "Like That",
+   "Result": "Won",
+   "Ref.": "[227]"
+ },
+ {
+   "Organization": "BMI R&B/Hip-Hop Awards",
+   "Year": 2021,
+   "Category": "Song of the Year",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[228]"
+ },
+ {
+   "Organization": "BMI R&B/Hip-Hop Awards",
+   "Year": 2021,
+   "Category": "Most Performed R&B/Hip-Hop Songs",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[228]"
+ },
+ {
+   "Organization": "BMI R&B/Hip-Hop Awards",
+   "Year": 2021,
+   "Category": "Most Performed R&B/Hip-Hop Songs",
+   "Nominated work": "Like That",
+   "Result": "Won",
+   "Ref.": "[228]"
+ },
+ {
+   "Organization": "BRIT Awards",
+   "Year": 2021,
+   "Category": "International Artist of the Year",
+   "Nominated work": "Herself",
+   "Result": "Pending",
+   "Ref.": "[229]"
+ },
+ {
+   "Organization": "BRIT Awards",
+   "Year": 2021,
+   "Category": "International Song of the Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Pending",
+   "Ref.": "[229]"
+ },
+ {
+   "Organization": "GAFFA Awards (Denmark)",
+   "Year": 2021,
+   "Category": "Best International New Act",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[230]"
+ },
+ {
+   "Organization": "GAFFA Awards (Denmark)",
+   "Year": 2021,
+   "Category": "International Solo Artist of the Year",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[231]"
+ },
+ {
+   "Organization": "GAFFA Awards (Denmark)",
+   "Year": 2021,
+   "Category": "International Album of the Year",
+   "Nominated work": "Hot Pink",
+   "Result": "Nominated",
+   "Ref.": "[231]"
+ },
+ {
+   "Organization": "GAFFA Awards (Denmark)",
+   "Year": 2021,
+   "Category": "International Song of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[231]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2021,
+   "Category": "Best New Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[119]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2021,
+   "Category": "Best Pop Solo Performance",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[119]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2021,
+   "Category": "Record of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[119]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Record of the Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Song of The Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Best Pop Duo/Performance",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Album of The Year",
+   "Nominated work": "Montero (as a featured artist)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Album of The Year",
+   "Nominated work": "Planet Her (Deluxe)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Best Pop Vocal Album",
+   "Nominated work": "Planet Her (Deluxe)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Best Melodic Rap Performance",
+   "Nominated work": "Need to Know",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Grammy Awards",
+   "Year": 2022,
+   "Category": "Best Rap Song",
+   "Nominated work": "Best Friend (with Saweetie)",
+   "Result": "Pending",
+   "Ref.": "[163]"
+ },
+ {
+   "Organization": "Guinness World Records",
+   "Year": 2021,
+   "Category": "First female rap duo to reach No.1 on the US singles chart",
+   "Nominated work": "Say So (featuring Nicki Minaj)",
+   "Result": "Won",
+   "Ref.": "[232]"
+ },
+ {
+   "Organization": "iHeartRadio Music Awards",
+   "Year": 2021,
+   "Category": "Best New Pop Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[233]"
+ },
+ {
+   "Organization": "iHeartRadio Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Music Video Choreography",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[233]"
+ },
+ {
+   "Organization": "iHeartRadio Music Awards",
+   "Year": 2021,
+   "Category": "TikTok Bop of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[233]"
+ },
+ {
+   "Organization": "Latin American Music Awards",
+   "Year": 2021,
+   "Category": "Favorite Video",
+   "Nominated work": "Del Mar (with Ozuna and Sia)",
+   "Result": "Nominated",
+   "Ref.": "[234]"
+ },
+ {
+   "Organization": "LOS40 Music Awards",
+   "Year": 2020,
+   "Category": "Best International New Act",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[235]"
+ },
+ {
+   "Organization": "LOS40 Music Awards",
+   "Year": 2021,
+   "Category": "Best International Act",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[236]"
+ },
+ {
+   "Organization": "MOBO Awards",
+   "Year": 2021,
+   "Category": "Best International Act",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[237]"
+ },
+ {
+   "Organization": "MTV Millennial Awards",
+   "Year": 2021,
+   "Category": "Global Hit of the Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[238]"
+ },
+ {
+   "Organization": "MTV Millennial Awards Brazil",
+   "Year": 2020,
+   "Category": "Global Hit",
+   "Nominated work": "Say So (featuring Nicki Minaj)",
+   "Result": "Nominated",
+   "Ref.": "[239]"
+ },
+ {
+   "Organization": "MTV Millennial Awards Brazil",
+   "Year": 2020,
+   "Category": "International Collaboration",
+   "Nominated work": "Say So (featuring Nicki Minaj)",
+   "Result": "Nominated",
+   "Ref.": "[239]"
+ },
+ {
+   "Organization": "MTV Millennial Awards Brazil",
+   "Year": 2021,
+   "Category": "International Collaboration",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[240]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2020,
+   "Category": "Best Push Act",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[101]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2020,
+   "Category": "Best New Act",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[101]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best US Act",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best Pop",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best Song",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best Video",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Europe Music Awards",
+   "Year": 2021,
+   "Category": "Best Collaboration",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Won",
+   "Ref.": "[241]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2020,
+   "Category": "Push Best New Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[90]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2020,
+   "Category": "Song of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[90]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2020,
+   "Category": "Best Direction",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[90]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2020,
+   "Category": "Song of Summer",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[90]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Video of the Year",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[149]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Best Collaboration",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Won",
+   "Ref.": "[149]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Best Art Direction",
+   "Nominated work": "Best Friend (with Saweetie)",
+   "Result": "Won",
+   "Ref.": "[149]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Best Visual Effects",
+   "Nominated work": "You Right (with The Weeknd)",
+   "Result": "Nominated",
+   "Ref.": "[149]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Artist of the Year",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[149]"
+ },
+ {
+   "Organization": "MTV Video Music Awards",
+   "Year": 2021,
+   "Category": "Song of Summer",
+   "Nominated work": "Need to Know",
+   "Result": "Nominated",
+   "Ref.": "[242]"
+ },
+ {
+   "Organization": "MTV Video Music Awards Japan",
+   "Year": 2020,
+   "Category": "Best New International Artist Video",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[243]"
+ },
+ {
+   "Organization": "MTV Video Play Awards",
+   "Year": 2020,
+   "Category": "Top 10 Music Videos",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[244]"
+ },
+ {
+   "Organization": "MTV Video Play Awards",
+   "Year": 2021,
+   "Category": "Top 20 Music Videos",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Won",
+   "Ref.": "[245]"
+ },
+ {
+   "Organization": "MVPA Awards",
+   "Year": 2021,
+   "Category": "Best Pop Video",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[246]"
+ },
+ {
+   "Organization": "MVPA Awards",
+   "Year": 2021,
+   "Category": "Best Cinematography in a Video",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[246]"
+ },
+ {
+   "Organization": "MVPA Awards",
+   "Year": 2021,
+   "Category": "Best Colour Grading in a Video",
+   "Nominated work": "Say So",
+   "Result": "Won",
+   "Ref.": "[247]"
+ },
+ {
+   "Organization": "MVPA Awards",
+   "Year": 2021,
+   "Category": "Best Lockdown Video",
+   "Nominated work": "To Be Young (with Anne-Marie)",
+   "Result": "Nominated",
+   "Ref.": "[246]"
+ },
+ {
+   "Organization": "NAACP Image Awards",
+   "Year": 2021,
+   "Category": "Outstanding New Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[248]"
+ },
+ {
+   "Organization": "NRJ Music Awards",
+   "Year": 2020,
+   "Category": "Video of the Year",
+   "Nominated work": "Say So",
+   "Result": "Nominated",
+   "Ref.": "[249]"
+ },
+ {
+   "Organization": "NRJ Music Awards",
+   "Year": 2020,
+   "Category": "Best New International Artist",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[249]"
+ },
+ {
+   "Organization": "NRJ Music Awards",
+   "Year": 2021,
+   "Category": "International Female Artist of the Year",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[250]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2020,
+   "Category": "Best New Artist of 2020",
+   "Nominated work": "Herself",
+   "Result": "Won",
+   "Ref.": "[102]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2020,
+   "Category": "Soundtrack Song of 2020",
+   "Nominated work": "Boss Bitch",
+   "Result": "Nominated",
+   "Ref.": "[102]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2021,
+   "Category": "The Female Artist of 2021",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[251]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2021,
+   "Category": "The Album of 2021",
+   "Nominated work": "Planet Her",
+   "Result": "Nominated",
+   "Ref.": "[251]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2021,
+   "Category": "The Collaboration Song of 2021",
+   "Nominated work": "Best Friend (with Saweetie)",
+   "Result": "Nominated",
+   "Ref.": "[251]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2021,
+   "Category": "The Collaboration Song of 2021",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[251]"
+ },
+ {
+   "Organization": "People's Choice Awards",
+   "Year": 2021,
+   "Category": "The Collaboration Song of 2021",
+   "Nominated work": "You Right (with The Weeknd)",
+   "Result": "Nominated",
+   "Ref.": "[251]"
+ },
+ {
+   "Organization": "Soul Train Music Awards",
+   "Year": 2021,
+   "Category": "Album of the Year",
+   "Nominated work": "Planet Her",
+   "Result": "Nominated",
+   "Ref.": "[252]"
+ },
+ {
+   "Organization": "Soul Train Music Awards",
+   "Year": 2021,
+   "Category": "Best R&B/Soul Female Artist",
+   "Nominated work": "Herself",
+   "Result": "Nominated",
+   "Ref.": "[252]"
+ },
+ {
+   "Organization": "Soul Train Music Awards",
+   "Year": 2021,
+   "Category": "Best Collaboration",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Nominated",
+   "Ref.": "[252]"
+ },
+ {
+   "Organization": "TEC Awards",
+   "Year": 2021,
+   "Category": "Outstanding Creative Achievement – Record Production/Album",
+   "Nominated work": "Hot Pink",
+   "Result": "Nominated",
+   "Ref.": "[253]"
+ },
+ {
+   "Organization": "UK Music Video Awards",
+   "Year": 2021,
+   "Category": "Best R&B/Soul Video – International",
+   "Nominated work": "Streets",
+   "Result": "Nominated",
+   "Ref.": "[254]"
+ },
+ {
+   "Organization": "Urban Music Awards",
+   "Year": 2021,
+   "Category": "Artist of the Year (USA)",
+   "Nominated work": "Herself",
+   "Result": "Pending",
+   "Ref.": "[255]"
+ },
+ {
+   "Organization": "Urban Music Awards",
+   "Year": 2021,
+   "Category": "Best Collaboration",
+   "Nominated work": "Kiss Me More (featuring SZA)",
+   "Result": "Pending",
+   "Ref.": "[255]"
+ }
+],
+    "genres": ["Pop", "R&B", "hip hop", "pop rap"],
+    "labels": ["RCA", "Kemosabe"],
+    "associated_acts": ["Y2K", "Yeti Beats", "Ariana Grande", "Saweetie"],
+    "website": "https://dojacat.com"
+}
