0a3c76b1ee5055b69733c0ff9d891dd98636058e
enciphered-nft <encipherednft@gmail.com>
2022-01-08 00:01:27 +0400

    ed sheeran

---
+++ assets/ed_sheeran/data.py
@@ -0,0 +1,21 @@
+import datetime
+
+METADATA = {
+    "name": "Edward Christopher Sheeran",
+    "born": datetime.datetime(1991, 2, 17),
+    "occupation": ["Singer-songwriter", "musician", "actor"],
+    "years_active": "2004-present",
+    "children": 1,
+    "total_award_wins": 121,
+    "total_nominations": 338,
+    "awards": "https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Ed_Sheeran",
+    "genres": ["Pop", "folk-pop", "soft rock"],
+    "instruments": ["Vocals", "guitar"],
+    "labels": ["Asylum", "Atlantic", "Elektra"],
+    "associated_acts": ["Benny Blanco", "Jake Gosling", "Fred Again", "Steve Mac", "Justin Bieber", "Taylor Swift", "Eminem", "Yelawolf", "Stormzy", "Amy Wadge", "Anne Marie", "Sir Elton John"],
+    "website": "https://edsheeran.com",
+    "facebook": "https://www.facebook.com/EdSheeranMusic",
+    "instagram": "https://www.instagram.com/teddysphotos",
+    "twitter": "https://twitter.com/edsheeran",
+    "spotify": "https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V"
+}
