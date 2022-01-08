1778077bcabc708ca9381f7c22d9af3c1dbe9372
enciphered-nft <encipherednft@gmail.com>
2022-01-08 22:17:39 +0400

    elon musk

---
+++ assets/elon_musk/data.py
@@ -0,0 +1,25 @@
+import datetime
+
+METADATA = {
+    "name": "Elon Reeve Musk",
+    "short_description": "Elon Reeve Musk is an entrepreneur and business magnate. He is the founder, CEO and Chief Engineer at SpaceX; early-stage investor, CEO and Product Architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. With an estimated net worth of around US$304 billion as of January 2022, Musk is the wealthiest person in the world.",
+    "born": datetime.datetime(1971, 6, 28),
+    "citizenship": ["South Africa", "Canada", "United States"],
+    "education": "University of Pennsylvania (BS, BA)",
+    "companies": [
+        "SpaceX",
+        "Tesla, Inc.",
+        "The Boring Company",
+        "X.com",
+        "Paypal",
+        "Neuralink",
+        "OpenAI",
+        "Zip2"
+    ],
+    "awards": "https://en.wikipedia.org/wiki/List_of_awards_and_honors_received_by_Elon_Musk",
+    "children": 7,
+    "parents": {"father": "Errol Musk", "mother": "Maye Musk"},
+    "siblings": {"sister": "Tosca Musk", "brother": "Kimbal Musk"},
+    "twitter": "https://twitter.com/elonmusk",
+    
+}
