e9d229007385f276f9a590f298280deeb5439e1e
enciphered-nft <encipherednft@gmail.com>
2022-01-10 00:12:45 +0400

    steve jobs

---
+++ assets/steve_jobs/data.py
@@ -0,0 +1,19 @@
+import datetime
+
+METADATA = {
+    "name": "Steven Paul Jobs",
+    "short_description": "Steven Paul Jobs (/dʒɒbz/; February 24, 1955 – October 5, 2011) was an American business magnate, industrial designer, investor, and media proprietor. He was the chairman, chief executive officer (CEO), and co-founder of Apple Inc.; the chairman and majority shareholder of Pixar; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and the founder, chairman, and CEO of NeXT. Jobs is widely recognized as a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak.",
+    "born": datetime.datetime(1955, 2, 24),
+    "died": datetime.datetime(2011, 10, 5),
+    "cause_of_death": "Neuroendocrine cancer",
+    "resting_place": "Alta Mesa Memorial Park",
+    "occupation": "Entrepreneurindustrial designerinvestormedia proprietor",
+    "years_active": "1976–2011",
+    "known_for": "Pioneer of the personal computer revolution with Steve WozniakCo-creator of the Apple II, Macintosh, iPod, iPhone, iPad, and first Apple Stores",
+    "title": "Co-founder, chairman and CEO of Apple Inc.Co-founder, primary investor and chairman of PixarFounder, chairman and CEO of NeXT",
+    "board_member_of": "The Walt Disney Company Apple Inc.",
+    "spouse(s)": "Laurene Powell \u200b(m.\xa01991)\u200b",
+    "partner(s)": "Chrisann Brennan (1972–1977)",
+    "children": 4,
+    "relatives": "Mona Simpson (sister)",
+}
