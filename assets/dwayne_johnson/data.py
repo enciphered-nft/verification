cae079d529cb75f46e3ccf23dcd25029b91eb625
enciphered-nft <encipherednft@gmail.com>
2022-01-10 22:30:18 +0400

    dwayne johnson

---
+++ assets/dwayne_johnson/data.py
@@ -0,0 +1,23 @@
+import datetime
+
+METADATA = {
+    "name": "Dwayne Douglas Johnson",
+    "ring_name": "The Rock",
+    "short_description": "Dwayne Douglas Johnson (born May 2, 1972), also known by his ring name The Rock, is an American actor, businessman, and former professional wrestler. Regarded as one of the greatest professional wrestlers of all time, he wrestled for WWE for eight years prior to pursuing an acting career. His films have grossed over $3.5 billion in North America and over $10.5 billion worldwide, making him one of the world's highest-grossing and highest-paid actors.",
+    "born": datetime.datetime(1972, 5, 2),
+    "citizenship": ["American", "Canadian"],
+    "alma_mater": "University of Miami",
+    "occupation": ["Actor", "producer", "professional wrestler", "businessman"],
+    "years_active": "1999-present",
+    "awards": "https://en.wikipedia.org/wiki/Dwayne_Johnson#Awards_and_honors",
+    "children": 3,
+    "height": "6 ft 5 in (196 cm)",
+    "weight": "260 lb (118 kg)",
+    "debut": "1996",
+    "retired": "2019",
+    "facebook": "https://www.facebook.com/DwayneJohnson",
+    "instagram": "https://www.instagram.com/therock",
+    "twitter": "https://twitter.com/TheRock",
+    "imdb": "https://www.imdb.com/name/nm0425005/",
+    "wikipedia": "https://en.wikipedia.org/wiki/Dwayne_Johnson"
+}
