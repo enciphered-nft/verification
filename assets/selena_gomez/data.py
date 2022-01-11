5f427e7e2dc7f860916491d897911f5e0315b569
enciphered-nft <encipherednft@gmail.com>
2022-01-12 00:00:07 +0400

    selena gomez

---
+++ assets/selena_gomez/data.py
@@ -0,0 +1,18 @@
+import datetime
+
+METADATA = {
+    "name": "Selena Marie Gomez",
+    "short_description": "Selena Marie Gomez; born July 22, 1992) is an American singer, actress, and producer. Born and raised in Texas, Gomez began her acting career on the children's television series Barney & Friends (2002-2004). In her teenage years, she rose to prominence for her lead role as Alex Russo in the Disney Channel television series Wizards of Waverly Place (2007-2012).",
+    "born": datetime.datetime(1992, 7, 22),
+    "occupation": ["Singer", "actress", "producer"],
+    "years_active": "2002-present",
+    "awards": "https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Selena_Gomez",
+    "genres": ["Pop", "dance-pop", "electropop"],
+    "instruments": ["Vocals"],
+    "labels": ["Walt Disney", "Hollywood", "Interscope"],
+    "associated_acts": ["Selena Gomez & the Scene", "Julia Michaels", "Rock Mafia", "DJ Snake"],
+    "website": "https://selenagomez.com",
+    "facebook": "https://www.facebook.com/Selena",
+    "instagram": "https://www.instagram.com/selenagomez",
+    "twitter": "https://twitter.com/selenagomez"
+}
