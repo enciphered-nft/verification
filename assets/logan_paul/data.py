f4e88992cfcc5d8ff88a37cb667b171207a46788
enciphered-nft <encipherednft@gmail.com>
2022-01-16 15:23:20 +0400

    logan paul

---
+++ assets/logan_paul/data.py
@@ -0,0 +1,32 @@
+import datetime
+
+METADATA = {
+    "name": "Logan Alexander Paul",
+    "born": datetime.datetime(1995, 4, 1),
+    "short_description": "Born and raised in Ohio, Logan moved to Los Angeles at 19 in order to pursue entertainment beyond social media. Over the past three years, he's garnered a massive audience and has become one of the most recognized social media stars on the planet.",
+    "maverick_the_parrot": "Arguably the world's best sidekick, Maverick the Parrot has been with Logan since day one. He's a Sun Conure who likes flying and eating peanut butter. He is also the first bird in history to be captured in virtual reality.",
+    "education": "Westlake High School",
+    "occupation": ["YouTuber", "Social Media Influencer", "Actor", "Director", "Podcast Host", "Boxer"],
+    "years_active": "2007-present",
+    "genres": ["Comedy", "Vlog"],
+    "social_reach": {
+        "total": 66100000,
+        "twitter": 5100000,
+        "instagram": 20000000,
+        "facebook": 16000000,
+        "snapchat": 2000000
+    },
+    "youtube_channel_total_views": {
+        "Logan Paul": 5872633796,
+        "TheOfficialLoganPaul": 636000000,
+        "IMPAULSIVE": 436000000,
+    },
+    "boxing": {
+        "nickname": "The Maverick",
+        "weight": "Cruiserweight",
+        "height": "6 ft 2 in (188 cm)",
+        "reach": "76 in (193 cm)",
+        "stance": "orthodox"
+    },
+    "website": "https://loganpaul.com"
+}
